96c14e3cdd414c283c503fe21f6c73a9337b3665
AzharAR <muhammad.azhar.arifin@gmail.com>
2022-09-18 21:04:24 +0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,8 +1,30 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = function(parameter){  
+  return Promise.all ([
+    promiseTheaterIXX(), 
+    promiseTheaterVGC()
+    
+  ]).then(function(data){
+    //console.log(data[0])
+    jumlahTotal = 0
+    data[0].forEach(element => {
+      if (element.hasil == parameter) jumlahTotal++
+    });
+    
+    data[1].forEach(element => {
+      if (element.hasil == parameter) jumlahTotal++
+    });
+    //console.log(jumlahTotal)
+    return jumlahTotal
+    
+  })
+
+  
+};
 
 module.exports = {
   promiseOutput,
 };
+
